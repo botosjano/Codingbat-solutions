236d3aff888687273a11ed33a894a48812c50955
Botos János <botosjano@gmail.com>
2020-10-22 16:54:45 +0200

    Map-1 finished

---
+++ src/com/company/Map_1.java
@@ -0,0 +1,98 @@
+package com.company;
+
+import java.util.Map;
+
+public class Map_1 {
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if(map.get("a")!= null){
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if(map.containsKey("a")){
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            map.put("ab", map.get("a")+map.get("b"));
+        }
+        return map;
+    }
+
+    public Map<String, String> topping1(Map<String, String> map) {
+        if(map.containsKey("ice cream")){
+            map.put("ice cream", "cherry");
+        }
+        map.put("bread", "butter");
+        return map;
+    }
+
+    public Map<String, String> topping2(Map<String, String> map) {
+        if(map.containsKey("ice cream")){
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if(map.containsKey("spinach")){
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+    public Map<String, String> topping3(Map<String, String> map) {
+        if(map.containsKey("potato")){
+            map.put("fries", map.get("potato"));
+        }
+        if(map.containsKey("salad")){
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB2(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            if(map.get("a").equals(map.get("b"))){
+                map.remove("a");
+                map.remove("b");
+            }
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        boolean doesAHaveValue = map.containsKey("a");
+        boolean doesBHaveValue = map.containsKey("b");
+
+        if(doesAHaveValue && !doesBHaveValue){
+            map.put("b", map.get("a"));
+        }
+        if(!doesAHaveValue && doesBHaveValue){
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")){
+            int aLength = map.get("a").length();
+            int bLength = map.get("b").length();
+
+            if(aLength > bLength){
+                map.put("c", map.get("a"));
+            }else if(bLength > aLength){
+                map.put("c", map.get("b"));
+            }else{
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
+
+
+}
